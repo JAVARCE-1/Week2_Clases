348bccbee20cc46e6776b2c047435462820e32ef
JAVARCE-1 <jaw.arce@gmail.com>
2025-04-16 21:32:56 -0500

    Subir proyecto con ejemplo de clase abstract e interface

---
+++ OPP_ABSTRACT_INTERFACES/Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_ABSTRACT_INTERFACES
+{
+    class  abstract class Vehiculo
+    {
+        public abstract void TocarClaxon()
+        {
+        }
+
+    }
+}
